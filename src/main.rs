132b4bdb92e785c6b6d834901f7e204ab4c0aa59
TreeMuffins <ben.m.clayton.2003@gmail.com>
2023-11-05 12:06:51 +0000

    window that doesn't work

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
-use sdl2::event::Event;
-
-use crate::winsdl::Winsdl;
-
-mod winsdl;
+use winit::{event_loop::EventLoop, event::WindowEvent};
+use winit::window::WindowBuilder;
 
 fn main() {
-    let mut winsdl = Winsdl::new(200,200).unwrap();
+    let event_loop = EventLoop::new();
+    let window_builder = WindowBuilder::new()
+        .with_title("My Window")
+        .with_inner_size(winit::dpi::LogicalSize::new(800.0, 600.0));
+    let window = window_builder.build(&event_loop).unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = winit::event_loop::ControlFlow::Poll;
+
+        match event {
+            winit::event::Event::WindowEvent { event, .. } => match event {
+                WindowEvent::KeyboardInput { input, .. } => {
+                    match input.virtual_keycode {
+                        Some(winit::event::VirtualKeyCode::Escape) => *control_flow = winit::event_loop::ControlFlow::Exit,
+                        _ => (),
+                    }
+                },
+                WindowEvent::CloseRequested => *control_flow = winit::event_loop::ControlFlow::Exit,
+                _ => (),
+            },
+            _ => (),
+        }
+    });
 }
